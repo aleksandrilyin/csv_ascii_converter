0d017f0dcb8ebdbb26a36e815fa692062d605819
Alexander Ilyin <alexander.ilyin11@gmail.com>
2021-07-27 11:53:33 +0300

    Use terminal-table

--- script.rb
+++ script.rb
@@ -1,10 +1,19 @@
 require 'smarter_csv'
+require 'terminal-table'
 
 filename = 'public/sample.csv'
-options = {
-  col_sep: ';'
-}
+options = { col_sep: ';' }
+rows = []
 
-data = SmarterCSV.process(filename, options).each do |element|
-  p element
+data = SmarterCSV.process(filename, options).each do |d|
+  rows << d.values
 end
+
+table = Terminal::Table.new do |t|
+  t.rows = rows
+  t.align_column(0, :right)
+  t.align_column(2, :right)
+  t.style = { all_separators: true, padding_left: 0, padding_right: 0 }
+end
+
+puts table

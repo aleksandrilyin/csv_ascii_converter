2026478ad5e3582b2c3f5c080b25155c37f328e0
Alexander Ilyin <alexander.ilyin11@gmail.com>
2021-08-20 23:21:01 +0300

    Update the money converter class

--- lib/money_converter.rb
+++ lib/money_converter.rb
@@ -1,16 +1,17 @@
 class MoneyConverter
+  NDIGITS = 2
+
   def self.convert(value)
-    split_value = value.to_s.split('.')
+    split_value = value.round(NDIGITS).to_s.split('.')
     integer_part, fractional_part = split_value
     new_integer_part = integer_part.reverse.gsub(/.{3}(?=.)/, '\0 ').reverse
-    numeric_array = fractional_part.split(//)
 
-    if numeric_array[1].nil?
-      new_fractional_part = numeric_array[0] + '0'
-    elsif numeric_array[1] < '5' && !numeric_array[2].nil?
-      new_fractional_part = numeric_array[0] + (numeric_array[1].to_i + 1).to_s
+    if fractional_part.size == NDIGITS
+      new_fractional_part = fractional_part
+    elsif integer_part == '0' && fractional_part == '0' && value != 0
+      new_fractional_part = "#{fractional_part}1"
     else
-      new_fractional_part = numeric_array[0] + numeric_array[1]
+      new_fractional_part = "#{fractional_part}0"
     end
 
     [new_integer_part, new_fractional_part].join(',')

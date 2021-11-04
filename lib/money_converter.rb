50055f5f1b750a2ffae2e5335f583e0475133e11
Alexander Ilyin <alexander.ilyin11@gmail.com>
2021-11-04 22:29:25 +0300

    Rename const

--- lib/money_converter.rb
+++ lib/money_converter.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class MoneyConverter
-  NDIGITS = 2
+  NDIGITS_AFTER_POINT = 2
 
   def self.convert(value)
-    integer_part, fractional_part = value.round(NDIGITS).to_s.split(".")
+    integer_part, fractional_part = value.round(NDIGITS_AFTER_POINT).to_s.split(".")
 
     new_integer_part = integer_part.reverse.gsub(/.{3}(?=.)/, "\\0 ").reverse
 
-    new_fractional_part = if fractional_part.size == NDIGITS
+    new_fractional_part = if fractional_part.size == NDIGITS_AFTER_POINT
       fractional_part
     elsif integer_part == "0" && fractional_part == "0" && value != 0
       "#{fractional_part}1"
